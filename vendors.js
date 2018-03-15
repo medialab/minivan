34e62de4d26d7eb6995804d83738d922c4b962ae
Yomguithereal <guillaumeplique@gmail.com>
2018-03-15 17:05:31 +0100

    Bundling

---
+++ vendors.js
@@ -0,0 +1,17 @@
+// Style
+require('angular-material/angular-material.css');
+
+// Angular
+require('angular');
+require('angular-animate');
+require('angular-aria');
+require('angular-material');
+require('angular-route');
+require('angular-sanitize');
+require('angular-marked');
+
+// Other libraries
+window.d3 = require('d3');
+window.Sigma = require('sigma/endpoint');
+window.Graph = require('graphology');
+// window.Graph.library = require('graphology-library');
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+  entry: './vendors.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
