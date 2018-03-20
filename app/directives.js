9616a422b9a80cb1e77bb4ef3e16c54e463628b0
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-03-20 09:35:11 +0100

    project title as directive

--- app/directives.js
+++ app/directives.js
@@ -4,4 +4,12 @@
 
 angular.module('app.directives', [])
 
-
+.directive('projectTitleBar', function(
+  ){
+    return {
+      restrict: 'E',
+      scope: {
+      },
+      templateUrl: 'components/projectTitleBar.html'
+    }
+  })
