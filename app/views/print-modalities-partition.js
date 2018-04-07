5d97560e7ffc8bba59623474d5ce87934e10f01a
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-04-07 12:56:09 +0200

    print modalities partition list

---
+++ app/views/print-modalities-partition.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('app.print-modalities-partition', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/print-modalities-partition', {
+    templateUrl: 'views/print-modalities-partition.html'
+  , controller: 'PrintModalitiesPartitionController'
+  })
+}])
+
+.controller('PrintModalitiesPartitionController', function(
+	$scope,
+	$location,
+	networkData
+) {
+	$scope.networkData = networkData
+	$scope.printMode = true
+	$scope.attributeId = $location.search().att
+
+	$scope.$watch('networkData.loaded', function(){
+		if ($scope.networkData.loaded) {
+			$scope.attribute = $scope.networkData.nodeAttributesIndex[$scope.attributeId]
+		}
+	})
+	
+	$scope.modalityListDetailLevel = $location.search().detail || 1
+	if ($scope.modalityListDetailLevel != 1 && $scope.modalityListDetailLevel != 2 && $scope.modalityListDetailLevel != 3) {
+		$scope.modalityListDetailLevel = 1
+	}
+})
