9e249b817dc9d30f283a51c4e46e301bbc16e21e
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-04-10 08:36:29 +0200

    prepare print ranking print modalities

---
+++ app/views/print-modalities-ranking.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('app.print-modalities-ranking', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/print-modalities-ranking', {
+    templateUrl: 'views/print-modalities-ranking.html'
+  , controller: 'PrintModalitiesRankingController'
+  })
+}])
+
+.controller('PrintModalitiesRankingController', function(
+	$scope,
+	$location,
+	networkData
+) {
+	$scope.networkData = networkData
+	$scope.printMode = true
+	$scope.attributeId = $location.search().att
+
+	$scope.$watch('networkData.loaded', function(){
+		if ($scope.networkData.loaded) {
+			$scope.attribute = $scope.networkData.nodeAttributesIndex[$scope.attributeId]
+			$scope.maxModCount = d3.max($scope.attribute.modalities.map(function(mod){ return mod.count }))
+		}
+	})
+	
+	$scope.modalityListDetailLevel = $location.search().detail || 1
+	if ($scope.modalityListDetailLevel != 1 && $scope.modalityListDetailLevel != 2 && $scope.modalityListDetailLevel != 3) {
+		$scope.modalityListDetailLevel = 1
+	}
+})
