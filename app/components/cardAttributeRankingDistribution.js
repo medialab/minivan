b8a44cd28321ee82c54f8843fec5d1b59f7996f2
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-04-10 12:16:41 +0200

    work on distribution chart

--- app/components/cardAttributeRankingDistribution.js
+++ app/components/cardAttributeRankingDistribution.js
@@ -22,21 +22,20 @@ angular.module('app.components.cardAttributeRankingDistribution', [])
   }
 })
 
-.directive('ranking-distribution-chart', function(
+.directive('rankingDistributionChart', function(
     $timeout
   ){
     return {
       restrict: 'A',
       scope: {
-        data: '=',
+        attribute: '=',
         printMode: '=',
         modalitiesSelection: '='
       },
       link: function($scope, el, attrs) {
-
         el.html('<div>LOADING</div>')
 
-        $scope.$watch('data', redraw)
+        $scope.$watch('attribute', redraw)
 
         window.addEventListener('resize', redraw)
         $scope.$on('$destroy', function(){
@@ -44,17 +43,43 @@ angular.module('app.components.cardAttributeRankingDistribution', [])
         })
 
         function redraw() {
-          if ($scope.data !== undefined){
+          if ($scope.attribute !== undefined){
             $timeout(function(){
               el.html('');
-              draw(d3.select(el[0]), $scope.data)
+              draw(d3.select(el[0]), $scope.attribute)
             })
           }
         }
 
-        function draw(container, data) {
+        function draw(container, attribute) {
+        	var values = g.nodes().map(function(nid){ return g.getNodeAttribute(nid, attribute.id) })
+        	var valuesExtent = d3.extent(values)
+        	var startFromZero = 0 <= valuesExtent[0]/valuesExtent[1] && valuesExtent[0]/valuesExtent[1] < 0.2
+        	var lowerRoundBound
+        	if (startFromZero) {
+        		lowerRoundBound = 0
+        	} else {
+	        	lowerRoundBound = Math.pow(10, Math.floor(Math.log(valuesExtent[0])/Math.log(10)))
+        	}
+        	var upperRoundBound = Math.pow(10, Math.ceil(Math.log(valuesExtent[1])/Math.log(10)))
+        	var maxBandsCount = 50
+        	var bandWidth = (upperRoundBound - lowerRoundBound) / maxBandsCount
+        	console.log(bandWidth)
+        	// Lower the bandWidth to closest round number
+        	bandWidth = Math.pow(10, Math.floor(Math.log(bandWidth)/Math.log(10)))
+        	console.log(bandWidth)
+        	// Rise it again to round multiple
+        	if ( (upperRoundBound - lowerRoundBound) / (5 * bandWidth) < maxBandsCount ) {
+        		bandWidth *= 5
+        	} else if( (upperRoundBound - lowerRoundBound) / (2 * bandWidth) < maxBandsCount ) {
+						bandWidth *= 2
+        	}
+
+/*        	var x = d3.scaleQuantize()
+        		.domain(startFromZero ? [0, valuesExtent[1]] : valuesExtent)
+        		.nice()
+*/
         	// TODO
-        	
 				}
       }
     }
