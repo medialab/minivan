cc219e8d6479c4fb7206857a20b1714fb8f12b4b
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-04-11 16:51:31 +0200

    distribution with scaleBand (not satisfying)

--- app/components/cardAttributeRankingDistribution.js
+++ app/components/cardAttributeRankingDistribution.js
@@ -46,12 +46,17 @@ angular.module('app.components.cardAttributeRankingDistribution', [])
           if ($scope.attribute !== undefined){
             $timeout(function(){
               el.html('');
-              draw(d3.select(el[0]), $scope.attribute)
+              draw(el[0], $scope.attribute)
             })
           }
         }
 
         function draw(container, attribute) {
+
+        	var settings = {}
+	        settings.maxBandsCount = container.offsetWidth < 400 ? 30 : 50
+	        settings.bar_spacing = 3
+
         	var values = g.nodes().map(function(nid){ return g.getNodeAttribute(nid, attribute.id) })
         	var valuesExtent = d3.extent(values)
         	var startFromZero = 0 <= valuesExtent[0]/valuesExtent[1] && valuesExtent[0]/valuesExtent[1] < 0.2
@@ -62,24 +67,74 @@ angular.module('app.components.cardAttributeRankingDistribution', [])
 	        	lowerRoundBound = Math.pow(10, Math.floor(Math.log(valuesExtent[0])/Math.log(10)))
         	}
         	var upperRoundBound = Math.pow(10, Math.ceil(Math.log(valuesExtent[1])/Math.log(10)))
-        	var maxBandsCount = 50
-        	var bandWidth = (upperRoundBound - lowerRoundBound) / maxBandsCount
-        	console.log(bandWidth)
+        	var bandWidth = (upperRoundBound - lowerRoundBound) / settings.maxBandsCount
         	// Lower the bandWidth to closest round number
         	bandWidth = Math.pow(10, Math.floor(Math.log(bandWidth)/Math.log(10)))
-        	console.log(bandWidth)
-        	// Rise it again to round multiple
-        	if ( (upperRoundBound - lowerRoundBound) / (5 * bandWidth) < maxBandsCount ) {
-        		bandWidth *= 5
-        	} else if( (upperRoundBound - lowerRoundBound) / (2 * bandWidth) < maxBandsCount ) {
-						bandWidth *= 2
+        	// Rise it up to round multiple
+        	if ( (upperRoundBound - lowerRoundBound) / (bandWidth*2) <= settings.maxBandsCount ) {
+        		bandWidth *= 2
+        	} else if( (upperRoundBound - lowerRoundBound) / (bandWidth*5) <= settings.maxBandsCount ) {
+						bandWidth *= 5
+        	} else {
+        		bandWidth *= 10
+        	}
+        	lowerRoundBound -= lowerRoundBound%bandWidth
+        	if (upperRoundBound%bandWidth > 0) {
+	        	upperRoundBound += bandWidth - upperRoundBound%bandWidth
+         	}
+        	var data = []
+        	var i
+        	for (i=lowerRoundBound; i<upperRoundBound; i += bandWidth) {
+        		var d = {}
+        		d.min = i
+        		d.max = i + bandWidth
+        		d.average = (d.min+d.max)/2
+        		d.count = values.filter(function(v){
+        			return v >= d.min && (v<d.max || ( i==upperRoundBound-bandWidth && v<=d.max ))
+        		}).length
+        		data.push(d)
         	}
 
-/*        	var x = d3.scaleQuantize()
-        		.domain(startFromZero ? [0, valuesExtent[1]] : valuesExtent)
-        		.nice()
-*/
-        	// TODO
+        	// set the dimensions and margins of the graph
+					var margin = {top: 12, right: 6, bottom: 48, left: 6},
+					    width = container.offsetWidth - margin.left - margin.right,
+					    height = container.offsetHeight - margin.top - margin.bottom;
+
+        	var x = d3.scaleBand()
+        		.domain(data.map(function(d) { return d.average }))
+        		.range([0, width])
+
+        	var y = d3.scaleLinear()
+	        	.domain(d3.extent(data, function(d){ return d.count }))
+	        	.range([0, height]);
+
+        	var svg = d3.select(container).append('svg')
+					    .attr('width', width + margin.left + margin.right)
+					    .attr('height', height + margin.top + margin.bottom)
+					  .append('g')
+					    .attr('transform', 
+					          'translate(' + margin.left + ',' + margin.top + ')');
+
+					// append the rectangles for the bar chart
+				  var bars = svg.selectAll('.bar')
+				      .data(data)
+
+				  bars.enter().append('rect')
+				      .attr('class', 'bar')
+				      .attr('x', function(d) { return x(d.average) } )
+				      .attr('y', function(d) { return height-y(d.count)} )
+				      .attr('height', function(d) { return y(d.count) })
+				      .attr('width', x.bandwidth() - settings.bar_spacing)
+				      .attr('fill', 'rgba(160, 160, 160, 0.5)')
+
+				  var xAxis = d3.axisBottom(x)
+				  	.tickValues(x.domain().filter(function(d,i){ return !(i%5)}));
+				  
+				  svg.append("g")
+			      .attr("class", "axis axis--x")
+			      .attr("transform", "translate(0," + height + ")")
+			      .call(xAxis);
+
 				}
       }
     }
