4d100ff634bb643be0550c23676baf5ca884462f
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-04-13 16:55:57 +0200

    secondary alphabetical sort for partitions

--- app/components/matrix.js
+++ app/components/matrix.js
@@ -74,6 +74,7 @@ angular.module('app.components.matrix', [])
         // Init view box
         updateViewBox()
 
+        // Nodes color and size
         if ($scope.selectedAttId) {
           if ($scope.att) {
             if ($scope.att.type == 'partition') {
@@ -84,7 +85,14 @@ angular.module('app.components.matrix', [])
               $scope.nodes.sort(function(a, b){
                 var aModIndex = modalitiesIndex[g.getNodeAttribute(a, $scope.selectedAttId)]
                 var bModIndex = modalitiesIndex[g.getNodeAttribute(b, $scope.selectedAttId)]
-                return aModIndex - bModIndex
+                var diff = aModIndex - bModIndex
+                if (diff == 0) {
+                  var alabel = g.getNodeAttribute(a, 'label')
+                  var blabel = g.getNodeAttribute(b, 'label')
+                  if (alabel > blabel) return 1
+                  else if (alabel < blabel) return -1
+                  return 0
+                } else return diff
               })
             } else if ($scope.att.type == 'ranking-size' || $scope.att.type == 'ranking-color') {
               $scope.nodes.sort(function(a, b){
