415ef6d35b43cbc066c90fe063d701294cb90969
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-07-17 19:27:11 +0200

    Refacto top nodes model

--- app/views/modality.js
+++ app/views/modality.js
@@ -35,6 +35,7 @@ angular.module('app.modality', ['ngRoute'])
         console.error('[ERROR] The type of attribute "' + $scope.attribute.name + '" is not "partition".', $scope.attribute)
       }
       updateNodeFilter()
+      buildAllSortedNodes()
     }
   })
   
@@ -65,6 +66,128 @@ angular.module('app.modality', ['ngRoute'])
     var blob = new Blob([csv], {'type':'text/csv;charset=utf-8'});
     saveAs(blob, $scope.networkData.title + " - " + $scope.modality.value + " - Nodes.csv");
   }
+
+  // Stats top nodes
+  $scope.statsTopCut
+  $scope.sortedNodes = {
+    inside: {
+      citedFromInside: [],
+      citedFromOutside: [],
+      pointingToInside: [],
+      pointingToOutside: [],
+      connectedInside: [],
+      connectedOutside: []
+    },
+    outside: {
+      citedFromInside: [],
+      citedFromOutside: [],
+      pointingToInside: [],
+      pointingToOutside: [],
+      connectedInside: [],
+      connectedOutside: []
+    }
+  }
+
+  $scope.$watch('statsDetailLevel', buildAllSortedNodes)
+
+  function buildAllSortedNodes(){
+    if (!$scope.attribute && !$scope.modality) { return }
+    var g = networkData.g
+    $scope.statsTopCut = ($scope.statsDetailLevel > 1) ? (10) : (3)
+    if (g.type == 'directed' || g.type == 'mixed') {
+      $scope.sortedNodes.inside.citedFromInside = buildSortedNodes('INSIDE', 'CITED_FROM', 'INSIDE')
+      $scope.sortedNodes.inside.citedFromOutside = buildSortedNodes('INSIDE', 'CITED_FROM', 'OUTSIDE')
+      $scope.sortedNodes.inside.pointingToInside = buildSortedNodes('INSIDE', 'POINTING_TO', 'INSIDE')
+      $scope.sortedNodes.inside.pointingToOutside = buildSortedNodes('INSIDE', 'POINTING_TO', 'OUTSIDE')
+      $scope.sortedNodes.outside.citedFromInside = buildSortedNodes('OUTSIDE', 'CITED_FROM', 'INSIDE')
+      // $scope.sortedNodes.outside.citedFromOutside = buildSortedNodes('OUTSIDE', 'CITED_FROM', 'OUTSIDE')
+      $scope.sortedNodes.outside.pointingToInside = buildSortedNodes('OUTSIDE', 'POINTING_TO', 'INSIDE')
+      // $scope.sortedNodes.outside.pointingToOutside = buildSortedNodes('OUTSIDE', 'POINTING_TO', 'OUTSIDE')
+    }
+    if (g.type == 'undirected' || g.type == 'mixed') {
+      $scope.sortedNodes.inside.connectedInside = buildSortedNodes('INSIDE', 'CONNECTED', 'INSIDE')
+      $scope.sortedNodes.inside.connectedOutside = buildSortedNodes('INSIDE', 'CONNECTED', 'OUTSIDE')
+      $scope.sortedNodes.outside.connectedInside = buildSortedNodes('OUTSIDE', 'CONNECTED', 'INSIDE')
+      // $scope.sortedNodes.outside.connectedOutside = buildSortedNodes('OUTSIDE', 'CONNECTED', 'OUTSIDE')
+    }
+  }
+
+  function buildSortedNodes(pool, mode, extremity_pool) {
+    var result = g.nodes()
+    var pool_condition
+    var extremity_pool_condition
+
+    // Filter pool
+    if (pool == 'INSIDE') {
+      pool_condition = function(nid) {
+        return g.getNodeAttribute(nid, $scope.attribute.id) == $scope.modality.value
+      }
+    } else {
+      pool_condition = function(nid) {
+        return g.getNodeAttribute(nid, $scope.attribute.id) != $scope.modality.value
+      }
+    }
+    result = result.filter(function(nid){
+      return pool_condition(nid)
+    })
+
+    // Results items
+    result = result.map(function(nid){
+      return {
+        id: nid,
+        node: g.getNodeAttributes(nid)
+      }
+    })
+
+    // Compute edge scores
+    if (extremity_pool == 'INSIDE') {
+      extremity_pool_condition = function(nid) {
+        return g.getNodeAttribute(nid, $scope.attribute.id) == $scope.modality.value
+      }
+    } else {
+      extremity_pool_condition = function(nid) {
+        return g.getNodeAttribute(nid, $scope.attribute.id) != $scope.modality.value
+      }
+    }
+    if (mode == 'CITED_FROM') {
+      result.forEach(function(item){
+        item.score = g.inEdges(item.id)
+          .filter(function(eid){
+            return extremity_pool_condition(g.source(eid))
+          })
+          .length
+      })
+    } else if (mode == 'POINTING_TO') {
+      result.forEach(function(item){
+        item.score = g.outEdges(item.id)
+          .filter(function(eid){
+            return extremity_pool_condition(g.target(eid))
+          })
+          .length
+      })
+    } else {
+      result.forEach(function(item){
+        item.score = g.edges(item.id)
+          .filter(function(eid){
+            return extremity_pool_condition(g.opposite(eid))
+          })
+          .length
+      })
+    }
+    result = result.filter(function(item){ return item.score > 0 }) // Ignore if 0 edges
+    result.sort(function(a, b){
+      return b.score - a.score
+    })
+
+    if (result.length > $scope.statsTopCut) {
+      result = result.filter(function(item, i){
+        return i < $scope.statsTopCut || item.score == result[$scope.statsTopCut - 1].score
+      })
+    }
+    return result
+  }
+
+  // Other functions
 
   function updateNodeFilter() {
     if ($scope.attribute) {
