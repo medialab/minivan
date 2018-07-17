5fa5d0f7344a2047c5d5cb13e64f427bb3a7fc6d
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-07-17 16:07:00 +0200

    Modality stats basics

--- app/components/cardModalitySummary.js
+++ app/components/cardModalitySummary.js
@@ -15,7 +15,16 @@ angular.module('app.components.cardModalitySummary', [])
     link: function($scope, el, attrs) {
       var g = networkData.g
       $scope.attribute = networkData.nodeAttributesIndex[$scope.attId]
-    	$scope.modality = $scope.attribute.modalitiesIndex[$scope.modValue]
+      $scope.modality = $scope.attribute.modalitiesIndex[$scope.modValue]
+      $scope.modalityFlow = $scope.attribute.data.modalityFlow[$scope.modValue][$scope.modValue]
+      // Density
+      if (g.type == 'directed') {
+        $scope.density = Graph.library.metrics.density.directedDensity($scope.modality.count, $scope.modalityFlow.count)
+      } else if(g.type == 'undirected') {
+        $scope.density = Graph.library.metrics.density.undirectedDensity($scope.modality.count, $scope.modalityFlow.count)
+      } else if(g.type == 'mixed') {
+        $scope.density = Graph.library.metrics.density.mixedDensity($scope.modality.count, $scope.modalityFlow.count)
+      }
 	  }
   }
 })
