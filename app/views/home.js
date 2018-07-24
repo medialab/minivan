e30b033b5e38c514533276cc867495ebc425c433
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-07-24 08:24:49 +0200

    Front page buttons

--- app/views/home.js
+++ app/views/home.js
@@ -17,5 +17,11 @@ angular.module('app.home', ['ngRoute'])
 	networkData
 ) {
 	$scope.networkData = networkData
+
+	$scope.downloadNetwork = function() {
+  	var xml = Graph.library.gexf.write($scope.networkData.g);
+    var blob = new Blob([xml], {'type':'text/gexf+xml;charset=utf-8'});
+    saveAs(blob, $scope.networkData.title + ".gexf");
+  }
 		
 })
