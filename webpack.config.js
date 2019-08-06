5063cfa464b21fb80c1419f4cd48c92a0ca9f0b5
Yomguithereal <guillaumeplique@gmail.com>
2019-08-06 16:08:56 +0200

    Avoiding webpack cheap eval source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 module.exports = {
   mode: 'development',
   entry: './vendors.js',
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'app')
